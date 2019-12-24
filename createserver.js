d135e106a6b294467a1b0abad4562270e1ed5c88
anandhere8 <anandhere8@gmail.com>
2019-12-24 23:51:33 +0530

    process.exit()

--- createserver.js
+++ createserver.js
@@ -3,6 +3,10 @@ const http = require('http');
 var server = http.createServer((req,res)=>
 {
     console.log(req);
+    setTimeout(()=>
+    {
+        process.exit();
+    },6000)
 });
 
 server.listen(3000);
