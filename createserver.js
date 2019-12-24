981bf324c5a1707fd214a061c38488a34e830d0d
anandhere8 <anandhere8@gmail.com>
2019-12-25 00:00:29 +0530

    first page

--- createserver.js
+++ createserver.js
@@ -3,10 +3,15 @@ const http = require('http');
 var server = http.createServer((req,res)=>
 {
     console.log(req);
-    setTimeout(()=>
-    {
-        process.exit();
-    },6000)
+    res.write('<html>');
+    res.write('<head>');
+    res.write('<title> First page');
+    res.write('</title>'); 
+
+    res.write('</head>');
+    res.write('<body>');
+    res.write('</body>');
+    res.write('</html>');
 });
 
 server.listen(3000);
