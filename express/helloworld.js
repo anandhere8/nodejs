5449cf6373ce4db0fb3d8d5f8137b3a4a32a9371
anandhere8 <anandhere8@gmail.com>
2020-01-05 23:56:17 +0530

    about

--- express/helloworld.js
+++ express/helloworld.js
@@ -5,6 +5,10 @@ app.get('/home',(req,res)=>{
     res.send('Hello world');
 });
 
+app.get('/about',(req,res)=>{
+    res.send('This is about section');
+});
+
 app.listen(3000,()=>{
     console.log("Server is running");
 });
